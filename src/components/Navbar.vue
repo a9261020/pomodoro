57d00fce3e64e03670590fc5d6a9f684d0e6c254
James <a9261020@gmail.com>
2020-04-04 10:31:18 +0800

    nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,26 +1,42 @@
 <template>
-  <div class="layout">
-    <div class="item"></div>
-    <div class="item"></div>
-    <div class="item"></div>
-    <div class="item"></div>
-  </div>
+  <nav class="position-fixed">
+    <router-link
+      class="text-center border-circle ds-inline-block"
+      :class="{ 'nav-active': status === 'TaskList' }"
+      to="/"
+      @click.native="changePage('TaskList')"
+    >
+      <i class="fas fa-align-left"></i>
+    </router-link>
+    <router-link
+      class="text-center border-circle ds-inline-block mt-30"
+      :class="{ 'nav-active': status === 'Analysis' }"
+      to="/analysis"
+      @click.native="changePage('Analysis')"
+    >
+      <i class="fas fa-chart-line"></i>
+    </router-link>
+    <router-link class="text-center border-circle ds-inline-block mt-30" to="/">
+      <i class="far fa-bell"></i>
+    </router-link>
+    <router-link class="text-center border-circle ds-inline-block mt-30" to="/">
+      <i class="fas fa-cog"></i>
+    </router-link>
+    <h1>POMODORO</h1>
+  </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      status: "TaskList",
+    };
+  },
+  methods: {
+    changePage(input) {
+      this.status = input;
+    },
+  },
+};
 </script>
-
-<style scoped lang="scss">
-.layout {
-  display: flex;
-  flex-direction: column;
-  width: 15%;
-  .item {
-    height: 100px;
-    width: 100px;
-    background-color: gray;
-    margin: 10px;
-  }
-}
-</style>
