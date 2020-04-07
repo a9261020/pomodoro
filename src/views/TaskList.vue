8ebd128d997e695f1114c11408f05301ea61e1be
James <a9261020@gmail.com>
2020-04-07 22:15:03 +0800

    task list layout 80%

--- src/views/TaskList.vue
+++ src/views/TaskList.vue
@@ -1,8 +1,58 @@
 <template>
-  <div class="page">tasklist vue</div>
+  <div class="page">
+    <header>
+      <p class="text-left page__title">
+        TASK LIST
+        <button
+          class="btn ml-50 border-radius"
+          :class="{ 'btn-active': mode === 'undone' }"
+          @click="changeMode('undone')"
+        >
+          undone
+        </button>
+        <button
+          class="btn ml-10 border-radius"
+          :class="{ 'btn-active': mode === 'completed' }"
+          @click="changeMode('completed')"
+        >
+          completed
+        </button>
+      </p>
+    </header>
+
+    <!-- main -->
+    <main class="taskList">
+      <input
+        class="taskList-input ds-inline-block border-radius"
+        type="text"
+        placeholder="Add new task..."
+      />
+      <button class="taskList-btn text-center ml-20 mb-50">
+        <i class="fas fa-plus-circle"></i>
+        ADD
+      </button>
+      <ul>
+        <li class="taskList-item mb-20">
+          <input type="checkbox" name="" id="" />
+          <p class="ds-inline-block taskList-item-context">some task</p>
+          <i class="taskList-item-delete fas fa-trash-alt"></i>
+        </li>
+      </ul>
+    </main>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      mode: "undone",
+    };
+  },
+  methods: {
+    changeMode(mode) {
+      this.mode = mode;
+    },
+  },
+};
 </script>
-
