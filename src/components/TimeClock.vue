b443c1b0730ed58eaad0dc4e69598e5a612169b3
James <a9261020@gmail.com>
2020-04-17 17:00:28 +0800

    timeClock的顯示ok

--- src/components/TimeClock.vue
+++ src/components/TimeClock.vue
@@ -5,20 +5,47 @@
       <h3 class="ds-inline-block">{{ titleTime }}</h3>
     </header>
     <main class="taskTimer">
-      <h3 class="taskTimer-taskTitle">
-        選擇任務：
-        <p
-          class="ds-inline-block"
-          v-show="Object.keys(this.getIsChecked).length === 0"
-        >
-          無
+      <h3 class="taskTimer-taskTitle" v-show="taskTimer.totalSeconds !== 0">
+        <p class="ds-inline-block" v-show="checkIsNotEmpty">
+          尚未選擇任務
         </p>
-        <p class="ds-inline-block">{{ getIsChecked.taskTitle }}</p>
+        <p class="ds-inline-block" v-show="!checkIsNotEmpty">
+          {{ getIsChecked.taskTitle }}
+          <span v-show="!checkIsNotEmpty && !taskTimer.isStart">暫停</span>
+          <span v-show="!checkIsNotEmpty && taskTimer.isStart">進行中</span>
+        </p>
       </h3>
-      <h3 class="taskTimer-timer">{{ timerMinutes }}:{{ timerSeconds }}</h3>
-      <button class="taskTimer-btn" @click="startTask">
-        <p v-if="!taskTimer.isStart">START</p>
-        <p v-else>PAUSE</p>
+      <h3 class="taskTimer-taskTitle" v-show="taskTimer.totalSeconds === 0">
+        <p class="ds-inline-block" v-show="!checkIsNotEmpty">
+          <span v-show="!checkIsNotEmpty && !breakTimer.isStart">暫停</span>
+          <span v-show="!checkIsNotEmpty && breakTimer.isStart"
+            >請好好的休息</span
+          >
+        </p>
+      </h3>
+      <h3 class="taskTimer-timer">
+        <p v-show="taskTimer.totalSeconds !== 0">
+          {{ taskTimerMinutes }}:{{ taskTimerSeconds }}
+        </p>
+        <p v-show="taskTimer.totalSeconds === 0">
+          {{ breakTimerMinutes }}:{{ breakTimerSeconds }}
+        </p>
+      </h3>
+      <button
+        class="taskTimer-btn"
+        @click="startTask"
+        v-show="taskTimer.totalSeconds !== 0"
+      >
+        <p v-show="!taskTimer.isStart">START</p>
+        <p v-show="taskTimer.isStart">PAUSE</p>
+      </button>
+      <button
+        class="taskTimer-btn"
+        @click="startBreak"
+        v-show="taskTimer.totalSeconds === 0"
+      >
+        <p v-show="!breakTimer.isStart">START</p>
+        <p v-show="breakTimer.isStart">PAUSE</p>
       </button>
     </main>
   </div>
@@ -45,7 +72,7 @@ export default {
         totalSeconds: 0,
         isStart: false,
       },
-      start: "",
+      taskStart: "",
     };
   },
   methods: {
@@ -60,21 +87,21 @@ export default {
       if (!this.taskTimer.isStart) {
         this.taskTimer.isStart = true;
         this.$store.dispatch("timeModule/taskStart", true);
-        this.updateTaskTimer();
+        this.updatetaskTimer();
       } else {
         this.taskTimer.isStart = false;
         this.$store.dispatch("timeModule/taskStart", false);
         // 計時停止的方法
         // 參考：https://jd615645.github.io/2017/10/16/Vue.js%E4%B8%AD%E4%BD%BF%E7%94%A8setInterval%E3%80%81clearInterval%E3%80%81clearLnterval%E3%80%81setTimeout/index.html
-        clearInterval(this.start);
+        clearInterval(this.taskStart);
       }
     },
-    updateTaskTimer() {
-      this.start = setInterval(() => {
+    updatetaskTimer() {
+      this.taskStart = setInterval(() => {
         // 計時完之後會做這件事情
         if (this.taskTimer.totalSeconds === 0) {
           alert("任務完成");
-          clearInterval(this.start);
+          clearInterval(this.taskStart);
           this.taskTimer.isStart = false;
           this.$store.dispatch("timeModule/taskStart", false);
           this.$store.dispatch("tasklistModule/doneTask", this.getIsChecked.id);
@@ -84,6 +111,9 @@ export default {
         this.taskTimer.seconds =
           this.taskTimer.totalSeconds - this.taskTimer.minutes * 60;
       }, 1000);
+    },
+    startBreak() {
+      console.log("Hello");
     },
   },
   computed: {
@@ -106,23 +136,40 @@ export default {
         ? `AM ${hours} : ${minutes} : ${seconds}`
         : `PM ${hours - 12} : ${minutes} : ${seconds}`;
     },
-    timerSeconds() {
+    taskTimerSeconds() {
       return this.taskTimer.seconds < 10
         ? `0${this.taskTimer.seconds}`
         : this.taskTimer.seconds;
     },
-    timerMinutes() {
+    taskTimerMinutes() {
       return this.taskTimer.minutes < 10
         ? `0${this.taskTimer.minutes}`
         : this.taskTimer.minutes;
     },
+    breakTimerSeconds() {
+      return this.breakTimer.seconds < 10
+        ? `0${this.breakTimer.seconds}`
+        : this.breakTimer.seconds;
+    },
+    breakTimerMinutes() {
+      return this.breakTimer.minutes < 10
+        ? `0${this.breakTimer.minutes}`
+        : this.breakTimer.minutes;
+    },
+    checkIsNotEmpty() {
+      return Object.keys(this.getIsChecked).length === 0;
+    },
   },
   watch: {
-    getTotalSeconds() {
-      this.taskTimer.taskTime = this.getTask.taskTime;
-      this.taskTimer.totalSeconds = this.getTask.taskTotalSeconds;
-      this.taskTimer.minutes = this.getTask.taskTime;
-      this.taskTimer.seconds = 0;
+    getTask: {
+      // 監聽陣列的話必須寫成物件，並加入deep:true，才會去監聽每個物件、陣列的變化
+      handler() {
+        this.taskTimer.taskTime = this.getTask.taskTime;
+        this.taskTimer.totalSeconds = this.getTask.taskTotalSeconds;
+        this.taskTimer.minutes = this.getTask.taskTime;
+        this.taskTimer.seconds = 0;
+      },
+      deep: true,
     },
   },
   created() {
@@ -131,6 +178,10 @@ export default {
     this.taskTimer.totalSeconds = this.getTask.taskTotalSeconds;
     this.taskTimer.minutes = this.getTask.taskTime;
     this.taskTimer.taskTime = this.getTask.taskTime;
+    // 初始化breakTimer
+    this.breakTimer.breakTime = this.getBreak.breakTime;
+    this.breakTimer.totalSeconds = this.getBreak.breakTotalSeconds;
+    this.breakTimer.minutes = this.getBreak.breakTime;
   },
 };
 </script>
