d369ab1a1c5a67669767eb48f63a3ec6cb8af39b
James <a9261020@gmail.com>
2020-04-20 23:04:21 +0800

    ringtones layout 剩下btn-active還沒做

--- src/views/Ringtones.vue
+++ src/views/Ringtones.vue
@@ -3,6 +3,26 @@
     <header>
       <p class="text-left page__title">RINGTONES</p>
     </header>
+    <main>
+      <ul>
+        <li class="ringtones border-radius">
+          <span class="ringtones-item-check"></span>
+          <span class="ringtones-name">default</span>
+          <span class="ringtones-play">
+            <i class="fas fa-play-circle"></i>
+          </span>
+        </li>
+      </ul>
+
+      <audio id="audio-0" src="../assets/ring/default.mp3"></audio>
+      <audio id="audio-1" src="../assets/ring/bell.mp3"></audio>
+      <audio id="audio-2" src="../assets/ring/birds.mp3"></audio>
+      <audio id="audio-3" src="../assets/ring/classic.mp3"></audio>
+      <audio id="audio-4" src="../assets/ring/opening.mp3"></audio>
+      <audio id="audio-5" src="../assets/ring/whatFlash.mp3"></audio>
+      <audio id="audio-6" src="../assets/ring/alert.mp3"></audio>
+      <audio id="audio-7" src="../assets/ring/warning.mp3"></audio>
+    </main>
   </div>
 </template>
 
